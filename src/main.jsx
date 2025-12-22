febcdee4c03cecd9d42aa2c443d4bdbda30a2c42
Tajmilur Rahman <tajmilurrahman91@gmail.com>
2025-12-23 00:42:39 +0600

    project ready to start

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,9 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App.jsx";
-import { RouterProvider } from "react-router";
 import router from "./routes/router.jsx";
 import AuthProvider from "./provider/AuthProvider.jsx";
+import { RouterProvider } from "react-router";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
